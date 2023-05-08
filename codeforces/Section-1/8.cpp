4e9a10301e09ee58c42e481b9518dadb6ca097b4
Kaustubh <kaustubhthakur76@gmail.com>
2023-05-08 19:26:25 +0530

    DAY-2

--- codeforces/Section-1/8.cpp
+++ codeforces/Section-1/8.cpp
@@ -3,17 +3,22 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int n;
-        cin>>n;
-        vector<int>a(n);
-        for(int i=0;i<n;i++) cin>>a[i];
-        vector<int>b(n);
-        for(int i=0;i<n;i++) cin>>b[i];
-        
-        
+        cin >> n;
+        vector<int> a(n);
+        for (auto &x : a)
+        {
+            cin >> x;
+        }
+        long long ans = 0;
+        for (int i = 1; i < n; i++)
+        {
+            ans = max(ans, 1LL * a[i] * a[i - 1]);
+        }
+        cout << ans << '\n';
     }
     return 0;
 }
