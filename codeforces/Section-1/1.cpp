b1ddf41101d7c40b4823e2971aa7d52c6845e69b
Kaustubh <kaustubhthakur76@gmail.com>
2023-05-10 00:32:56 +0530

    DAY-1

---
+++ codeforces/Section-1/1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 10003;
+int a[N];
+int b[N];
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+            cin >> a[i];
+        for (int i = 1; i <= n; i++)
+            cin >> b[i];
+    
+    }
+    return 0;
+}
