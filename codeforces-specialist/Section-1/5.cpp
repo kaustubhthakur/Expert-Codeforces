b08b252ff2e657a425e172604d5ed6708c47cc9b
Kaustubh <kaustubhthakur76@gmail.com>
2023-05-15 23:37:43 +0530

    initial commit

---
+++ codeforces-specialist/Section-1/5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        std::cin >> N;
+        for (int i = 1; i <= N; i++)
+            cout << 2 * i << '\n';
+    }
+}
